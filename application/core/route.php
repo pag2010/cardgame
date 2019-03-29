dd36483adb242d753945e2199e447ac2761fbbe6
pag.2010 <pag.2010@ya.ru>
2019-03-29 20:17:11 +0300

    Добавил вывод информации по модели auth. Исправил ошибку с отображением

--- application/core/route.php
+++ application/core/route.php
@@ -29,7 +29,7 @@
     
             $model_file = strtolower($model_name).'.php';
             $model_path = "/app/application/models/".$model_file;
-            echo "file exists ".$model_path." ".file_exists($model_path);
+            echo "file exists ".$model_path." ".(file_exists($model_path) ?'true':'false');
             if(file_exists($model_path))
             {
                 include "/app/application/models/".$model_file;
