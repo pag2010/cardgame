5ab34057dc738592cf83284ccb1b35f13691a190
pag.2010 <pag.2010@ya.ru>
2019-03-30 11:23:34 +0300

    Добавил js с функцией хэширования пароля

--- application/controllers/controller_auth.php
+++ application/controllers/controller_auth.php
@@ -21,14 +21,14 @@
         }
         function action_login()
         {
-            $this->view->generate('auth/login_view.php', 'template_view.php');
+            $this->view->generate('auth/login_view.php', 'template_view.php', 'auth_js.php');
             if (isset($_POST['submit'])){
                 $this->model->login=$_POST['login'];
                 $err=$this->model->get_data();
                 if ($err!=null){ 
                     echo 'Введён неверный логин или пароль';
                 }else{
-                   if ((strcmp(hash ('sha256', $_POST['password']), $this->model->password_hash))==0){
+                   if ((strcmp($_POST['password'], $this->model->password_hash))==0){
                        $_SESSION['logged_in']=true;
                        $_SESSION['login']=$_POST['login'];
                         echo 'Авторизация пройдена успешно!';
