ac559a846137432483419780827d74fb3d4fd3e8
pag.2010 <pag.2010@ya.ru>
2019-04-07 18:41:44 +0300

    Добавил файлы

---
+++ application/views/chat/create_chat_view.php
@@ -0,0 +1,10 @@
+<form name="create_chat" onsubmit="return onSubmitClick()" method="Post">
+<table>
+    <tr>
+    <td>Логин</td> <td><input required name="login" type="text"></td>
+    </tr>
+    <tr>
+    <td><input name="submit" type="submit" value="Создать чат"></td>
+    </tr>
+</table>
+</form>
---
+++ application/views/chat/opened_chat_view.php
@@ -0,0 +1,8 @@
+<form name="make_msg" onsubmit="return onSubmitClick()" method="POST">
+<table>
+    <tr>
+    <td><input required name="msg" type="text"></td>
+    <td><input name="submit" type="submit" value="Отправить"></td>
+    </tr>
+</table>
+</form>
